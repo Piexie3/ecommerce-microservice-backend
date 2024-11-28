c02af3687a58db7f0b6b94381adff66e40445d9e
piexie3 <emmanuelkbett3@gmail.com>
2024-11-28 13:42:25 +0300

    product service initialization and configuration

---
+++ src/product/src/test/java/com/appaces/ecommerce/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appaces.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
